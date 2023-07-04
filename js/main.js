55f67dda70c0430cb8314e66494470a7074092bb
Solvey Mear <soleil.mear@hotmail.fr>
2023-07-04 18:52:57 +0200

    essai header + main-img

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import "../scss/fonts.scss";
 import "../scss/generic-style.scss"
 //import "../scss/buttons.scss";
 //import "../scss/header.scss";
-import "../scss/main-img2/scss"
+import "../main-img2/scss"
 //import "../scss/main-img.scss";
 //import "../scss/push.scss";
 //import "../scss/products.scss";

ed9efeea7091c1a419a11a8eba3ea4d053ea260a
mymyoux <mymyoux@gmail.com>
2023-07-05 11:12:26 +0200

    t

--- pages/header-main-img.php
+++ pages/header-main-img.php
@@ -1,3 +1,6 @@
+<?php 
+require_once __DIR__ . '/../vitephp/helpers.php';
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -49,7 +52,7 @@
     </div>
     
     </section>
-    <script type="module" src="../js/main.js"></script>
-
+    <!-- <script type="module" src="../js/main.js"></script> -->
+    <?= vite('js/main.js') ?>
 </body>
 </html>

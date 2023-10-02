ae2124eb1a64151283aa36439968bd5906dd366c
Solvey Mear <soleil.mear@hotmail.fr>
2023-10-02 17:50:15 +0200

    scrollTo js

---
+++ js/scrollTo.js
@@ -0,0 +1,10 @@
+let mainButton = document.querySelector('.buttons-main')
+
+mainButton.addEventListener('click', function() {
+    window.scrollTo({
+        top: 1030,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
